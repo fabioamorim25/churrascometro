0eb741d9af5f2b709e2bccb4f8081265e7e4c31f
fabio <fabiosantosamorim2@gmail.com>
2022-04-25 14:26:47 -0300

    calculadora: verificar os valores recebidos

--- codigos/model.js
+++ codigos/model.js
@@ -1,5 +1,48 @@
+//CALCULADORA
 
- 
+// LOGICA: RECEBER OS VALORES DIGITADOS PELO USUARIO (quant:crianças,adulto,duração)
+let adultos = document.getElementById("adulto")
+let criancas = document.getElementById("crianca")
+let tempo = document.getElementById("duracao")
+///////////////////////////////////////////////////////////////////////////////////////////
+//LOGICA: VERIFICAR OS VALORES RECEBIDOS DOS INPUTS (quant:crianças,adulto,duração) 
+function verificar() {
+    if (adultos.value <= 0 || criancas.value <= -1 || tempo.value <= 0) {
+        alert("valores invalidos");
+        adultos.value = 0;
+        criancas.value = 0;
+        tempo.value = 0;
+    } else {
+       // chamar as funçoes para gera os valores do relatorio 
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// LOGICA: DIVs CONTEINER ( onde sera armazenados os resultados encontrados) 
+let quant_TotalCerveja =document.getElementsByClassName("resultadoCerveja");//conteiner linha 71 html
+let quant_TotalCarne =document.getElementsByClassName("resultadoCarne");//conteiner linha 80 html
+let quant_TotalRefrigerante =document.getElementsByClassName("resultadoRefrigerante"); //conteiner linha 89 html
+
+////CALCULOS
+
+// LOGICA: CALCULAR A QUANTIDADE TOTAL DE CERVEJA
+
+// LOGICA: CALCULAR A QUANTIDADE TOTAL DE CARNE
+
+// LOGICA: CALCULAR A QUANTIDADE TOTAL DE REFRIGERANTE
+
+
+
+
+
+
+
+
+
+
 
 
 
--- codigos/script.js
+++ codigos/script.js
@@ -1,30 +1,27 @@
 // LOGICA: MODELO DE ORGANIZADOR (solo ou compartilhado) 
 //compartilhado- esconder o botão de imprimir da segunda pagina e deixar visivel a terceira pagina
 //solo- esconder a terceira pagina e esconder a seta para a pagina 3
-const buttonSolo=document.getElementById ("solo");
 const buttonCompartilhado=document.getElementById ("compartilhado");
-const buttonImprimir=document.getElementById ("imprimir_pdf");
+const buttonSolo=document.getElementById ("solo");
 
+const buttonImprimir=document.getElementById ('imprimir_pdf');
+
+ function modeloCompartilhado (){
+  document.getElementById ("imprimir_pdf").hidden=true;
+  document.getElementById ("conteiner_pagina3").hidden=false;
+  document.getElementById ("setaBaixo2").hidden=false;
+}
+function modeloSolo(){     
+  document.getElementById ("conteiner_pagina3").hidden=true;//ocultar toda a terceira pagina 
+  document.getElementById ("setaBaixo2").hidden=true;//ocultar o botão pra a proxima pagina
+  document.getElementById ("imprimir_pdf").hidden=false;//mostra o botão imprimir 
+}
+// LOGICA: MUDAR PARA A PROXIMA PAGINA QUANDO A SETA P/ BAIXO FOR USADA 
+// 1° seta- entra na segunda pagina // 2° seta- entra na terceira pagina // e os botões de navegação entra na respectiva função abaixo
 const buttonSeta2=document.getElementById ("setaBaixo2");
 const buttonSeta1=document.getElementById ("setaBaixo1");
 
-function modeloCompartilhado (){
-    
-    document.getElementById ("imprimir_pdf").hidden=true;
-    document.getElementById ("conteiner_pagina3").hidden=false;
-    document.getElementById ("setaBaixo2").hidden=false;
-}
-function modeloSolo (){     
-    document.getElementById ("conteiner_pagina3").hidden=true;//ocultar toda a terceira pagina 
-    document.getElementById ("setaBaixo2").hidden=true;//ocultar o botão pra a proxima pagina
-    document.getElementById ("imprimir_pdf").hidden=false;//mostra o botão imprimir 
-} 
-
-// LOGICA: MUDAR PARA A PROXIMA PAGINA QUANDO A SETA P/ BAIXO FOR USADA 
-// 1° seta- entra na segunda pagina // 2° seta- entra na terceira pagina
-
 function rolarPagina2(){
-//console.log("asdadasdas");
 var pagina2 = document.getElementById("setaLateral");
     pagina2.scrollIntoView();
 }
@@ -33,8 +30,30 @@ function rolarPagina3(){
     pagina3.scrollIntoView();
 }
 function rolarPagina1(){
-    //console.log("asdadasdas");
     var pagina1 = document.getElementById("menuSuperior");
     pagina1.scrollIntoView();
 }
+
+// LOGICA: REGRA PARA IR PARA A SEGUNDA PAGINA
+// deve escolher um dos modelos de organizador e clicar na 1° seta para baixo (caso não executar as duas regras, sera mantido na primeira pagina em espera)
+
+
+
+// LOGICA: TRAVAR A POSIÇÃO DO SCROLL DO MOUSE
+//usado para fazer com que o usuario tenha que utilizar o sistema de navegação do sistema
+
+
+
+
+
+
+
+
+
+
+// CONTROLE 1: RECEBER OS DADOS DOS INPUS DA CALCULADORA 
+//gera o relatorio generico das compras (total: carne,refrigerante,cerveja)
+
+
+
 
