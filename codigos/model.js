864e42d7525fdf70c66a823f712a55b8857a74cf
fabio <fabiosantosamorim2@gmail.com>
2022-04-26 15:10:05 -0300

    PREÇO DOS PRODUTOS: mostra os valores encontrados no relatorio de compras

--- codigos/model.js
+++ codigos/model.js
@@ -1,28 +1,34 @@
-//CALCULADORA
-
 // LOGICA: RECEBER OS VALORES DIGITADOS PELO USUARIO (quant:crianças,adulto,duração)
 let adultos = document.getElementById("adulto")
 let criancas = document.getElementById("crianca")
 let tempo = document.getElementById("duracao")
-///////////////////////////////////////////////////////////////////////////////////////////
+
+
 //LOGICA: VERIFICAR OS VALORES RECEBIDOS DOS INPUTS (quant:crianças,adulto,duração) 
-function verificar() {
+
+function verificar() {// usado quando temos valores estranhos como( nem um adultos no churrasco; tempo com valor zero; valores negativos)
     if (adultos.value <= 0 || criancas.value <= -1 || tempo.value <= 0) {
-        alert("valores invalidos");
+        alert("VALORES INVALIDOS: não sera aceito nem um adulto no churrasco; tempo com valor zero; valores negativos");
         adultos.value = 0;
         criancas.value = 0;
         tempo.value = 0;
     } else {
-        relatorioDasCompras () 
+      verificar2()
     }
+}
+function verificar2() { // usado para nao quebra os conteiner dos relatorio. quando tem valores alto demais 
+  if (adultos.value > 8888 || criancas.value > 8888 || tempo.value > 8888) {
+      alert("valores invalidos");
+      adultos.value = 0;
+      criancas.value = 0;
+      tempo.value = 0;
+  } else {
+      relatorioDasCompras () 
+  }
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 
-
- 
-
-
-                                                //ESTRUTURA PARA AS RESPOSTAS DO CONTEINER CALCULADORA
+                   //ESTRUTURA PARA AS RESPOSTAS DO CONTEINER CALCULADORA
 
 // LOGICA: CERVEJA
 function valorCerveja(tempo) {
@@ -63,15 +69,40 @@ function relatorioDasCompras (){
  
  
 // declarar o id de cada conteiner para colocar os valores da quantidade de "carne e bebidas" encontrados  
+    
+    //mostra o valor da Cerveja no conteiner (html Linha:69 )
     let conteinerCerveja =document.getElementById ("resultadoCerveja");
     conteinerCerveja.innerHTML= `<div>${qntGeral_cerveja} Uni.</div>`; 
-    
+    //mostra o valor da Cerveja no conteiner (html Linha:117 )
+    let conteinerCerveja2 =document.getElementById ("resultadoCerveja2");
+    conteinerCerveja2.innerHTML= `<div>${qntGeral_cerveja} Uni.</div>`; 
+
+    //mostra o valor da Carne no conteiner (html Linha:80 )
     let conteinerCarne =document.getElementById ("resultadoCarne");
     conteinerCarne.innerHTML= `<div>${qntGeral_carne} Kg</div>`;
-    
+    //mostra o valor da Carne no conteiner (html Linha:154 )
+    let conteinerCarne2 =document.getElementById ("resultadoCarne2");
+    conteinerCarne2.innerHTML= `<div>${qntGeral_carne} Kg</div>`;
+
+    //mostra o valor do Refrigerante no conteiner (html Linha:91 )
     let conteinerRefrigerante =document.getElementById ("resultadoRefrigerante");
-    conteinerRefrigerante.innerHTML= `<div>${qntGeral_refrigerante} Uni.</div>`; 
+    conteinerRefrigerante.innerHTML= `<div>${qntGeral_refrigerante} Uni.</div>`;
+    //mostra o valor do Refrigerante no conteiner (html Linha:136)
+    let conteinerRefrigerante2 =document.getElementById ("resultadoRefrigerante2");
+    conteinerRefrigerante2.innerHTML= `<div>${qntGeral_refrigerante} Uni.</div>`; 
 }
+ 
+
+                  //ESTRUTURA PARA COLOCAR OS PREÇOS DOS PRODUTOS
+
+
+
+
+
+
+
+
+
 
 
 
